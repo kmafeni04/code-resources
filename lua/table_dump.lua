0ff31d7fb2aa82052d71c35de9d59781ab3a8595
kmafeni04 <komemafeni944@gmail.com>
2024-12-23 13:53:13 +0100

    [lua] update table dump to add pretty priniting

--- lua/table_dump.lua
+++ lua/table_dump.lua
@@ -1,22 +1,28 @@
+---@class TableDump
+--- The table_dump module
+local table_dump = {}
 --[[
   Returns the contents of a table as a string
   Usage:
   ```lua
   local table_dump = require("table_dump")
   local tbl = { [1] = 10, hello = 1 }
-  print(table_dump(tbl))
+  print(table_dump.tostring(tbl))
   --> Prints { [1] = 10, ["hello"] = 1, }
   ```
 ]]
 ---@param tbl table
 ---@return string
-local function table_dump(tbl)
+function table_dump.tostring(tbl)
+  assert(type(tbl) == "table", "Param `tbl` must be of type `table`")
+  if not next(tbl) then
+    return "{}"
+  end
   local str = "{ "
   for key, value in pairs(tbl) do
     if type(value) == "table" then
-      value = table_dump(value)
-    end
-    if type(value) == "string" then
+      value = table_dump.tostring(value)
+    elseif type(value) == "string" then
       value = [["]] .. value .. [["]]
     end
     if type(key) ~= "number" then
@@ -24,7 +30,47 @@ local function table_dump(tbl)
     end
     str = str .. "[" .. key .. "] = " .. tostring(value) .. ", "
   end
-  return str .. " }"
+  return str .. "}"
+end
+
+--[[
+  Returns the contents of a table as a pretty string
+  Usage:
+  ```lua
+  local table_dump = require("table_dump")
+  local tbl = { [1] = 10, hello = 1 }
+  print(table_dump.pretty_tostring(tbl))
+  --> Prints 
+  {
+    [1] = 10,
+    ["hello"] = 1,
+  }
+  ```
+]]
+---@param tbl table
+---@param indent integer?
+---@return string
+function table_dump.pretty_tostring(tbl, indent)
+  assert(type(tbl) == "table", "Param `tbl` must be of type `table`")
+  if not next(tbl) then
+    return "{}"
+  end
+  indent = indent or 0
+  assert(type(indent) == "number", "Param `indent` must be of type `number`")
+  local indent_str = "  "
+  local str = "{\n " .. string.rep(indent_str, indent + 1)
+  for key, value in pairs(tbl) do
+    if type(value) == "table" then
+      value = table_dump.pretty_tostring(value, indent + 1)
+    elseif type(value) == "string" then
+      value = [["]] .. value .. [["]]
+    end
+    if type(key) ~= "number" then
+      key = '"' .. key .. '"'
+    end
+    str = str .. "[" .. key .. "] = " .. tostring(value) .. ",\n " .. string.rep(indent_str, indent + 1)
+  end
+  return str:sub(1, #str - #indent_str - 1) .. "}"
 end
 
 return table_dump
