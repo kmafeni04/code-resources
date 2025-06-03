7c846cb1482296c3775b0624c8299a30fe99417c
kmafeni04 <komemafeni944@gmail.com>
2025-06-03 16:04:09 +0100

    [lua] Add new config option in luamon to exclude dirs

--- lua/luamon.lua
+++ lua/luamon.lua
@@ -14,7 +14,11 @@ local function check_modification(root_dir, file_path, callback)
     local mod = file_attrs.modification
     if mod > current_time then
       current_time = mod
-      print(lfs.currentdir() .. "/" .. file_path .. " has been modified")
+      if file_path:match("^%./") then
+        print(lfs.currentdir() .. "/" .. file_path:sub(3) .. " has been modified")
+      else
+        print(file_path .. " has been modified")
+      end
       lfs.chdir(root_dir)
       callback()
     end
@@ -24,24 +28,32 @@ end
 --- For files without extensions or full file_names, an `_` can be used in front of the filename. e.g. `_lua` or `_luamon.lua`
 --- Fields `exclude_file_types` and `only_file_types` can not be present at the same time
 ---@class Config
----@field exclude_file_types? string[] An array of file types to be ignored when monitoring
----@field only_file_types? string[] An array of file types to only be monitored
+---@field exclude_file_types? string[] An array of file types to be ignored
+---@field include_file_types? string[] An array of file types to be monitored
+---@field exclude_dirs? string[] An array of directories to be ignored
 ---@field recursive? boolean Whether or not subdirectories should be checked, Default: `true`
 
----@param dir string?
+---@param root_dir string
+---@param dir string
 ---@param callback function
 ---@param config? Config
 local function check_dir(root_dir, dir, callback, config)
   local recursive = true
   for file_path in lfs.dir(dir) do
     if file_path ~= ".." and file_path ~= "." then
+      local file_attrs, _, _ = lfs.attributes(file_path)
       file_path = dir .. "/" .. file_path
       if config then
         assert(
-          not (config.exclude_file_types and config.only_file_types),
+          not (config.exclude_file_types and config.include_file_types),
           "`exclude_file_types` and `only_file_types` fields can not be present at the same time"
         )
         recursive = config.recursive and config.recursive or recursive
+        if file_attrs and file_attrs.mode == "directory" then
+          if config.exclude_dirs[file_path:sub(#root_dir + 2)] then
+            goto continue
+          end
+        end
         if config.exclude_file_types then
           for _, file_type in ipairs(config.exclude_file_types) do
             local file_match = (
@@ -57,8 +69,8 @@ local function check_dir(root_dir, dir, callback, config)
               break
             end
           end
-        elseif config.only_file_types then
-          for _, file_type in ipairs(config.only_file_types) do
+        elseif config.include_file_types then
+          for _, file_type in ipairs(config.include_file_types) do
             local file_match = (
               file_path:match("^.*%." .. file_type .. "$") or file_path == dir .. "/" .. file_type:sub(2)
             )
@@ -80,14 +92,14 @@ local function check_dir(root_dir, dir, callback, config)
       end
 
       if recursive then
-        local file_attrs, _, _ = lfs.attributes(file_path)
         if file_attrs and file_attrs.mode == "directory" then
           lfs.chdir(file_path)
           check_dir(root_dir, ".", callback, config)
-          lfs.chdir("..")
+          lfs.chdir(root_dir)
         end
       end
     end
+    ::continue::
   end
 end
 
@@ -116,23 +128,23 @@ end
   end, config)
   ```
 ]]
----@param directory? string
+---@param dir? string
 ---@param callback function
 ---@param config? Config
-local function luamon(directory, callback, config)
-  if directory then
-    assert(directory:match("^/.*"), "Directory must be an absolute path but was provided: '" .. directory .. "'")
+local function luamon(dir, callback, config)
+  if dir then
+    assert(dir:match("^/.*"), "Directory must be an absolute path but was provided: '" .. dir .. "'")
   else
     local err
-    directory, err = lfs.currentdir()
-    assert(directory, err)
+    dir, err = lfs.currentdir()
+    assert(dir, err)
   end
-  local changed_dir, err = lfs.chdir(directory)
+  local changed_dir, err = lfs.chdir(dir)
   assert(changed_dir, err)
   callback()
 
   while true do
-    check_dir(directory, directory, callback, config)
+    check_dir(dir, dir, callback, config)
   end
 end
 
